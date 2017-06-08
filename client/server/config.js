95ab3015d754c3272ebeffc56a17ceb359dd5d81
ch1bi <maine1191@gmail.com>
2017-06-08 00:54:15 -0400

    added authentication

--- client/server/config.js
+++ client/server/config.js
@@ -1,6 +1,6 @@
-module.exports = {
 
-    db:"localhost",
-    clientSecret: process.env.clientSecret || "1e9b1c1e726a469ba0080c7d55b5e04a",
-    tokenSecret: process.env.tokenSecret || "pick a hard secret"
+module.exports = {
+  db: process.env.db || 'localhost',
+  tokenSecret: process.env.tokenSecret || 'pick a hard to guess string'
+
 };
