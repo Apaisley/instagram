8f75b2190f844156ac6f54312b4de11406f9b66d
ch1bi <maine1191@gmail.com>
2017-06-18 12:08:56 -0400

    parts 15-16

--- client/server/server.js
+++ client/server/server.js
@@ -36,7 +36,7 @@ function createToken(user) {
 
 
 function isAuthenticated(req, res, next) {
-  if (!(req.headers &amp;amp;&amp;amp; req.headers.authorization)) {
+  if (!( req.headers.authorization)) {
     return res.status(400).send({ message: 'You did not provide a JSON Web Token in the Authorization header.' });
   }
 
@@ -45,7 +45,7 @@ function isAuthenticated(req, res, next) {
   var payload = jwt.decode(token, config.tokenSecret);
   var now = moment().unix();
 
-  if (now &amp;gt; payload.exp) {
+  if (payload.exp) {
     return res.status(401).send({ message: 'Token has expired.' });
   }
 
@@ -183,6 +183,49 @@ app.post('/auth/instagram', function(req, res) {
     }
   });
 });
+
+app.get('/api/feed', isAuthenticated, function(req, res) {
+  var feedUrl = 'https://api.instagram.com/v1/users/self/feed';
+  var params = { access_token: req.user.accessToken };
+
+  request.get({ url: feedUrl, qs: params, json: true }, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.send(body.data);
+    }
+  });
+});
+
+app.get('/api/media/:id', isAuthenticated, function(req, res, next) {
+  var mediaUrl = 'https://api.instagram.com/v1/media/' + req.params.id;
+  var params = { access_token: req.user.accessToken };
+
+  request.get({ url: mediaUrl, qs: params, json: true }, function(error, response, body) {
+    if (!error &amp;amp;&amp;amp; response.statusCode == 200) {
+      res.send(body.data);
+    }
+  });
+});
+
+
+app.post('/api/like', isAuthenticated, function(req, res, next) {
+  var mediaId = req.body.mediaId;
+  var accessToken = { access_token: req.user.accessToken };
+  var likeUrl = 'https://api.instagram.com/v1/media/' + mediaId + '/likes';
+
+  request.post({ url: likeUrl, form: accessToken, json: true }, function(error, response, body) {
+    if (response.statusCode !== 200) {
+      return res.status(response.statusCode).send({
+        code: response.statusCode,
+        message: body.meta.error_message
+      });
+    }
+    res.status(200).end();
+  });
+});
+
+
+
+
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
